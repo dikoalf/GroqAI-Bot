aabab524fbe28077d3d31b3a78668dc3e34e878e
dikoalf <dikoalfatha@gmail.com>
2024-06-28 21:07:41 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,65 @@
+import streamlit as st
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_groq import ChatGroq
+import fitz
+
+st.title("Grog Bot")
+
+# Inisialisasi ChatGrog
+key = "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"
+chat = ChatGroq(
+    temperature=0,
+    model="llama3-70b-8192",
+    api_key=key
+)
+
+# Membuat template percakapan
+system = "You are a helpful assistant."
+human = "{text}"
+groqPrompt = ChatPromptTemplate.from_messages([("system", system), ("human", human)])
+
+# Inisialisasi chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Menampilkan histori chat ketika ada prompt baru
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Fungsi untuk membaca PDF dan mengekstrak teks
+def readPDF(file):
+    doc = fitz.open(stream=file.read(), filetype="pdf")
+    text = ""
+    for page in doc:
+        text += page.get_text()
+    return text
+
+# Upload PDF file
+file = st.file_uploader("Upload PDF", type="pdf")
+if file:
+    fileContents = readPDF(file)
+
+# React to user input
+if grogInput := st.chat_input("Apa yang ingin Anda ketahui?"):
+    # Menampilkan pesan/prompt dari user
+    st.chat_message("user").markdown(grogInput)
+    # Menambahkan pesan/prompt user ke dalam chat history
+    st.session_state.messages.append({"role": "user", "content": grogInput})
+
+    # Menggabungkan teks PDF dengan input user
+    combinedInput = fileContents + "\n\n" + grogInput
+
+    response = groqPrompt | chat
+
+    try:
+        response = response.invoke({"text": combinedInput})
+        response = response.content
+    except:
+        response = "Maaf, permintaan anda tidak dapat dilakukan saat ini."
+
+    # Menampilkan response dari groq atau pencarian
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    # Menambahkan response groq ke dalam chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
